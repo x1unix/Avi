f4ec413d0cee42b1485f7b0f01dc596d45532288
odin3 <denis0051@gmail.com>
2016-10-07 19:41:47 +0300

    Created search bar

--- app/src/main/java/com/x1unix/avi/MainActivity.java
+++ app/src/main/java/com/x1unix/avi/MainActivity.java
@@ -1,10 +1,13 @@
 package com.x1unix.avi;
 
+import android.app.SearchManager;
 import android.net.Uri;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.support.v7.widget.SearchView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -20,9 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        // Retrieve the SearchView and plug it into SearchManager
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
 
