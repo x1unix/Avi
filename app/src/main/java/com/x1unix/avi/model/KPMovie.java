836a008cb8f291ffb6fe331d8850b04b691255a7
odin3 <denis0051@gmail.com>
2016-10-10 23:57:22 +0300

    Kinopoisk Movie Model

---
+++ app/src/main/java/com/x1unix/avi/model/KPMovie.java
@@ -0,0 +1,48 @@
+package com.x1unix.avi.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KPMovie {
+    @SerializedName("filmID")
+    public String id;
+
+    @SerializedName("nameRU")
+    public String nameRu;
+
+    @SerializedName("nameEN")
+    public String nameEn;
+
+    @SerializedName("description")
+    public String description;
+
+    @SerializedName("posterURL")
+    public String posterUrl;
+
+    @SerializedName("year")
+    public String year;
+
+    @SerializedName("filmLength")
+    public String duration;
+
+    @SerializedName("county")
+    public String country;
+
+    @SerializedName("genre")
+    public String genre;
+
+    public KPMovie(String kpId, String nameEn, String nameRu, String description, String posterUrl,
+                    String year, String duration, String country, String genre) {
+        this.id = kpId;
+        this.nameRu = nameRu;
+        this.nameEn = nameEn;
+        this.description = description;
+        this.posterUrl = posterUrl;
+        this.year = year;
+        this.duration = duration;
+        this.country = country;
+        this.genre = genre;
+    }
+}
