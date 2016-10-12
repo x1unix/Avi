24cfadf6f158bfbb0ffff89276a0f6d87867ea66
odin3 <denis0051@gmail.com>
2016-10-12 05:08:58 +0300

    Added Movie player activity

---
+++ app/src/main/java/com/x1unix/avi/MoviePlayerActivity.java
@@ -0,0 +1,13 @@
+package com.x1unix.avi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoviePlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_player);
+    }
+}
