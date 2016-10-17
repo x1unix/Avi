8edbe64a91df13de9091244c328231ac653520af
odin3 <denis0051@gmail.com>
2016-10-17 03:25:15 +0300

    Add fullscreen feature to video player activity

--- app/src/main/java/com/x1unix/avi/MoviePlayerActivity.java
+++ app/src/main/java/com/x1unix/avi/MoviePlayerActivity.java
@@ -1,12 +1,16 @@
 package com.x1unix.avi;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Point;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -19,6 +23,86 @@ public class MoviePlayerActivity extends AppCompatActivity {
     private WebView webView;
     private String LSECTION = "MoviePlayer";
     private String currentUrl = "";
+    private boolean movieLoaded = false;
+    private Intent receivedIntent;
+
+    // Begin Android FullScreen Routine
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static final int UI_ANIMATION_DELAY = 300;
+    private final Handler mHideHandler = new Handler();
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            // Delayed removal of status and navigation bar
+
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            webView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+            // Load movie player in webview after fullscreen mode entered
+            if (!movieLoaded) {
+                // Load player with intent data
+                if (receivedIntent != null) {
+                    loadPlayer(receivedIntent.getStringExtra("movieId"),
+                            receivedIntent.getStringExtra("movieTitle"));
+                }
+            }
+        }
+    };
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Delayed display of UI elements
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+        }
+    };
+    private boolean mVisible;
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+    // End
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +112,18 @@ public class MoviePlayerActivity extends AppCompatActivity {
 
         webView = (WebView) findViewById(R.id.webplayer);
 
-        Intent receivedIntent = getIntent();
+        mVisible = true;
 
-        // Load player with intent data
-        if (receivedIntent != null) {
-            loadPlayer(receivedIntent.getStringExtra("movieId"),
-                    receivedIntent.getStringExtra("movieTitle"));
-        }
+
+        // Set up the user interaction to manually show or hide the system UI.
+        webView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggle();
+            }
+        });
+
+        receivedIntent = getIntent();
     }
 
     private void loadPlayer(String kpId, String title) {
@@ -59,6 +148,7 @@ public class MoviePlayerActivity extends AppCompatActivity {
         });
 
         setMovieId(kpId);
+        movieLoaded = true;
     }
 
     public void setMovieId(String kpId) {
@@ -66,4 +156,58 @@ public class MoviePlayerActivity extends AppCompatActivity {
         Log.i(LSECTION, "Loading url: " + currentUrl);
         webView.loadUrl(currentUrl);
     }
+
+    // Begin Android FullScreen helpers
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+    private void toggle() {
+        if (mVisible) {
+            hide();
+        } else {
+            show();
+        }
+    }
+
+    private void hide() {
+        // Hide UI first
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable);
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void show() {
+        // Show the system bar
+        webView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        mVisible = true;
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable);
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    /**
+     * Schedules a call to hide() in [delay] milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+    // End
 }
