48eeed4fef0d6a3bb1f6ab3e07f8cc586a8f4ef0
odin3 <denis0051@gmail.com>
2016-10-29 00:19:02 +0300

    SettingsActivity extends from PreferencesActivity

--- app/src/main/java/com/x1unix/avi/SettingsActivity.java
+++ app/src/main/java/com/x1unix/avi/SettingsActivity.java
@@ -2,12 +2,15 @@ package com.x1unix.avi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.Preference;
 
-public class SettingsActivity extends AppCompatActivity {
+
+public class SettingsActivity extends AppCompatPreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_main);
         setContentView(R.layout.activity_settings);
     }
 }
