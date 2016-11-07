542dc2bf4ec25cf18bc76c61436a9de7e575f3a7
odin3 <denis0051@gmail.com>
2016-11-08 01:54:00 +0200

    Implemented OTA events listener

--- app/src/main/java/com/x1unix/avi/updateManager/OTAStateListener.java
+++ app/src/main/java/com/x1unix/avi/updateManager/OTAStateListener.java
@@ -3,8 +3,17 @@ package com.x1unix.avi.updateManager;
 import com.x1unix.avi.model.AviSemVersion;
 
 public class OTAStateListener {
-    public OTAStateListener() {
-
+    public OTAStateListener(boolean success, AviSemVersion receivedVersion) {
+        if (success) {
+            AviSemVersion current = AviSemVersion.getApplicationVersion();
+            if (current.isYoungerThan(receivedVersion)) {
+                onUpdateAvailable(receivedVersion, current);
+            } else {
+                onUpdateMissing(receivedVersion, current);
+            }
+        } else {
+            onError();
+        }
     }
 
     protected void onUpdateAvailable(AviSemVersion availableVersion, AviSemVersion currentVersion) {
