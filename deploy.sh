ea488cb66bad6068321f906dee33d80ced22930b
Denis Sedchenko <ascii@live.ru>
2016-11-09 16:27:42 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,10 @@ ENVIRONMENT=production
 LOCAL_USERNAME=`whoami`
 REVISION=`git log -n 1 --pretty=format:"%H"`
 
-curl https://api.rollbar.com/api/1/deploy/ \
+if [ "$TRAVIS_BRANCH" == "prod" ]; then
+  curl https://api.rollbar.com/api/1/deploy/ \
   -F access_token=$ACCESS_TOKEN \
   -F environment=$ENVIRONMENT \
   -F revision=$REVISION \
   -F local_username=$LOCAL_USERNAME
+fi
