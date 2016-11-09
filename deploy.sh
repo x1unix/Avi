a5abddf1af25a950f43069aa650e6c7128bc1add
Denis Sedchenko <ascii@live.ru>
2016-11-09 16:26:52 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+ACCESS_TOKEN=286659d0df564dfe996e17c7f45268dd
+ENVIRONMENT=production
+LOCAL_USERNAME=`whoami`
+REVISION=`git log -n 1 --pretty=format:"%H"`
+
+curl https://api.rollbar.com/api/1/deploy/ \
+  -F access_token=$ACCESS_TOKEN \
+  -F environment=$ENVIRONMENT \
+  -F revision=$REVISION \
+  -F local_username=$LOCAL_USERNAME
