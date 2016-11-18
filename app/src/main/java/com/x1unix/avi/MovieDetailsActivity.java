41ac4b725c60c0db6155c8563a5ff3788ea8b7b0
odin3 <denis0051@gmail.com>
2016-11-18 02:31:24 +0200

    Header of Movie Details Activity

--- app/src/main/java/com/x1unix/avi/MovieDetailsActivity.java
+++ app/src/main/java/com/x1unix/avi/MovieDetailsActivity.java
@@ -13,16 +13,6 @@ public class MovieDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
