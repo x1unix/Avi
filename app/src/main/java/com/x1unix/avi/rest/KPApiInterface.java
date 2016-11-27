bc2b887b26f2b37a1ec13abfa20754593a6ae2a2
odin3 <denis0051@gmail.com>
2016-11-28 00:38:32 +0200

    Remove unused class

--- app/src/main/java/com/x1unix/avi/rest/KPApiInterface.java
+++ app/src/main/java/com/x1unix/avi/rest/KPApiInterface.java
@@ -1,10 +1,8 @@
 package com.x1unix.avi.rest;
 
-import com.x1unix.avi.model.KPMovie;
 import com.x1unix.avi.model.KPMovieDetailViewResponse;
 import com.x1unix.avi.model.KPSearchResponse;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
