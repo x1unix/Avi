f440cc67f101710941f2c5ce60e77748779d9029
odin3 <denis0051@gmail.com>
2016-11-28 00:38:05 +0200

    Remove unused class

--- app/src/main/java/com/x1unix/avi/rest/KPRestClient.java
+++ app/src/main/java/com/x1unix/avi/rest/KPRestClient.java
@@ -1,11 +1,7 @@
 package com.x1unix.avi.rest;
 
-import com.x1unix.avi.kp.KinopoiskRequestInterceptor;
-
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+
 import com.kinopoisk.*;
 
 public class KPRestClient {
