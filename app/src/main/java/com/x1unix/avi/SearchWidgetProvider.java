7de5d56266dd30e974b594606c026b7835dc37e9
odin3 <ascii@live.ru>
2017-01-04 03:28:35 +0200

    Add widget markup

---
+++ app/src/main/java/com/x1unix/avi/SearchWidgetProvider.java
@@ -0,0 +1,12 @@
+package com.x1unix.avi;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class SearchWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+}
