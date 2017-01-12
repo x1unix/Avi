efccc268003ddacf1e899a78930144dd6db9da88
odin3 <ascii@live.ru>
2017-01-12 22:09:20 +0200

    Add AppMetrica

--- app/src/main/java/com/x1unix/avi/MainActivity.java
+++ app/src/main/java/com/x1unix/avi/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rollbar.android.Rollbar;
+import com.yandex.metrica.YandexMetrica;
 
 public class MainActivity extends AppCompatActivity {
 
