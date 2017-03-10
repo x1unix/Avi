6e97fd7b141c645920687212a66545c0f926d4d4
odin3 <ascii@live.ru>
2017-03-10 04:15:15 +0200

    [MW] Get playlist using session

--- app/src/main/java/com/x1unix/moonwalker/Grabber.java
+++ app/src/main/java/com/x1unix/moonwalker/Grabber.java
@@ -7,10 +7,13 @@ import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.JavaNetCookieJar;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import retrofit2.http.HEAD;
 
 public class Grabber {
     private String referrer;
@@ -31,6 +34,36 @@ public class Grabber {
     public String getPlayerScriptByKinopoiskId(String kpId) throws IOException, MoonException {
         String url = "http://moonwalk.co/player_api?kp_id=" + kpId;
         return this.getResource(url);
+    }
+
+
+    public String getPlaylist(MoonSession session) throws IOException, MoonException {
+
+        RequestBody formBody = new FormBody.Builder()
+                .add(MoonSession.JSON_PROP_VIDEO_TOKEN, session.getVideoToken())
+                .add(MoonSession.JSON_PROP_CONTENT_TYPE, session.getContentType())
+                .add(MoonSession.JSON_PROP_KEY, session.getKey())
+                .add(MoonSession.JSON_PROP_PID, session.getPid())
+                .add(MoonSession.JSON_PROP_P_DOMAIN_ID, session.getDomainId())
+                .add(MoonSession.JSON_PROP_AD_ATTR, session.getAdAttr())
+                .add(MoonSession.JSON_PROP_DEBUG, session.isDebug())
+                .build();
+
+        Request request = new Request.Builder()
+                .url(session.getHost() + SESSION_URL)
+                .addHeader(HEAD_CSRF_TOKEN, session.getCSRFToken())
+                .addHeader(HEAD_RAY, HEAD_RAY_VALUE)
+                .post(formBody)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        if (!response.isSuccessful()) {
+            int code = response.code();
+            throw new MoonException("Failed to get playlist using session, HTTP Code: " + code );
+        }
+
+        return response.body().string();
     }
 
     public String getResource(String url)  throws IOException, MoonException {
